ac8d61c3464db7ce825054ce999073655ba4c575
Zahar Litvinchuk <zahar@raynelz.ru>
2025-10-09 00:59:09 +0300

    Initial Commit

---
+++ CarsHw/CarsHwApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsHwApp.swift
+//  CarsHw
+//
+//  Created by Захар Литвинчук on 09.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct CarsHwApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
